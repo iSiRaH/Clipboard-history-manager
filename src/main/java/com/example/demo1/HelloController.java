b625b96c98ca8327a07b95e63db35dfaae930895
Nuwan <nuwanrathnayaka53@gmail.com>
2025-05-22 23:18:37 +0530

    database creating for store history 2

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.sql.SQLException;
+
 public class HelloController {
     @FXML
     private TextField searchField;
@@ -28,9 +30,22 @@ public class HelloController {
         clipboardListView.setItems(clipboardItems);
 
         // Start clipboard monitoring in a new thread
-        Thread clipboardThread = new Thread(new Clipboard_saver(clipboardItems));
-        clipboardThread.setDaemon(true);
-        clipboardThread.start();
+//        Thread clipboardThread = new Thread(new Clipboard_saver(clipboardItems));
+//        clipboardThread.setDaemon(true);
+//        clipboardThread.start();
+
+        copyButton.setOnAction(event -> onInsert());
+
+    }
+
+    private void onInsert() {
+        String str = searchField.getText();
+        ClipboardDatabase clipboardDatabase = new ClipboardDatabase();
+        try {
+            clipboardDatabase.insertClipboarditem(str);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     requires java.desktop;
     opens com.example.demo1 to javafx.fxml;
